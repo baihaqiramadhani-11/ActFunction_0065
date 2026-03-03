82169fb68c2b9ce4b21638ea5c59ed6e54f283aa
baihaqiramadhani-11 <baihakiramadhani66@gmail.com>
2026-03-03 16:27:16 +0700

    add input procedure

--- praktikum2.cpp
+++ praktikum2.cpp
@@ -2,3 +2,10 @@
 using namespace std;
 //deklarasi global
     float p,l;
+//implementasi prosedur
+void input (){
+    cout << "masukkan panjang : " ;
+    cin >> p;
+    cout << "masukkan lebar : " ;
+    cin >> l; 
+}
